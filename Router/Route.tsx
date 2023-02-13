0e9e7a9c7d1c932b9a95343667a3f107065f0867
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-13 16:03:09 +0200

    Update Route.tsx

--- Router/Route.tsx
+++ Router/Route.tsx
@@ -1,46 +1,57 @@
 import * as React from 'react';
+import { Button, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
-import Home from './../pages/home';
-import HomeScreen from "./../pages/HomeScreen";
-import Detalhes from './../pages/Detalhes';
+import Inicio from '../pages/inicio';
+import Sobre from '../pages/Sobre';
+import HomeScreen from '../pages/HomeScreen';
+import Home from '../pages/home';
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
-const Stack = createNativeStackNavigator();
-
-export default function TabsNavigation() {
-    const Tab = createBottomTabNavigator();
-
+function MyStack() {
     return (
         <NavigationContainer>
-            <Tab.Navigator
-                screenOptions={({ route }) => ({
-                    tabBarIcon: ({ focused, color, size }) => {
-                        let iconName;
-
-                        if (route.name === 'Home') {
-                            iconName = focused
-                                ? 'ios-server-outline'
-                                : 'ios-server-outline';
-                        } else if (route.name === 'Settings') {
-                            iconName = focused ? 'ios-list' : 'ios-list-outline';
-                        } else if (route.name === 'book') {
-                            iconName = focused ? 'ios-list' : 'ios-library-outline';
-                        }
-                        return <Ionicons name={iconName} size={size} color={color} />;
-                    },
-                    tabBarActiveTintColor: '#cd077d',
-                    tabBarInactiveTintColor: 'gray',
-                })}
-            >
-                <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-                <Tab.Screen name="book5" component={Home} options={{ headerShown: false }} />
-                <Tab.Screen name="Settings" component={Home} options={{ headerShown: false }} />
-            </Tab.Navigator>
+            <Stack.Navigator>
+                <Stack.Screen name="Inicio" component={TabsNavigation} />
+                <Stack.Screen name="Sobre" component={Sobre} />
+            </Stack.Navigator>
         </NavigationContainer>
     );
 }
 
 
+function TabsNavigation() {
+    const Tab = createBottomTabNavigator();
+    return (
 
+        <Tab.Navigator screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+                if (route.name === 'Home') {
+                    iconName = focused
+                        ? 'ios-server-outline'
+                        : 'ios-server-outline';
+                } else if (route.name === 'Settings') {
+                    iconName = focused ? 'ios-list' : 'ios-list-outline';
+                } else if (route.name === 'book') {
+                    iconName = focused ? 'ios-list' : 'ios-library-outline';
+                }
+                return <Ionicons name={iconName} size={size} color={color} />;
+            },
+
+            tabBarActiveTintColor: '#cd077d',
+            tabBarInactiveTintColor: 'gray',
+        })}
+        >
+            <Tab.Screen name="Home" component={Inicio} options={{ headerShown: false }} />
+            <Tab.Screen name="book" component={Home} options={{ headerShown: false }} />
+            <Tab.Screen name="Settings" component={Home} options={{ headerShown: false }} />
+        </Tab.Navigator>
+
+    );
+}
+
+export { MyStack };
