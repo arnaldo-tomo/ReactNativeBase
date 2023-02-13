440bf0ef174521e6be614bb5ebc756fa923cdda0
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-14 00:03:25 +0200

    Update StackNavigation.tsx

--- src/Navigation/StackNavigation.tsx
+++ src/Navigation/StackNavigation.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Button, Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Detalhes from "../../pages/Detalhes";
@@ -16,7 +15,7 @@ function MainStack() {
         <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen name="Tabs" component={TabNavigator} options={{ headerShown: false }} />
-                <Stack.Screen name="detalhes" component={Detalhes} />
+                <Stack.Screen name="Detalhes" component={Detalhes} />
                 <Stack.Screen name="Inicio" component={Inicio} />
             </Stack.Navigator>
         </NavigationContainer>
@@ -35,14 +34,18 @@ function TabNavigator() {
                         ? 'ios-server-outline'
                         : 'ios-server-outline';
                 } else if (route.name === 'Settings') {
-                    iconName = focused ? 'ios-list' : 'ios-list-outline';
+                    iconName = focused ?
+                        'ios-search-outline' :
+                        'ios-search-outline';
                 } else if (route.name === 'book') {
-                    iconName = focused ? 'ios-list' : 'ios-library-outline';
+                    iconName = focused ?
+                        'ios-library-outline' :
+                        'ios-library-outline';
                 }
                 return <Ionicons name={iconName} size={size} color={color} />;
             },
 
-            tabBarActiveTintColor: 'blue',
+            tabBarActiveTintColor: 'red',
             tabBarInactiveTintColor: 'gray',
         })}
         >
