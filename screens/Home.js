ddf3f57c4e590adb5cc703ab07c68f3508648a43
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-13 16:03:18 +0200

    Create Home.js

---
+++ screens/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { View, Button, Text, StyleSheet } from "react-native";
+
+const Home = () => {
+    return (
+        <View style={styles.center}>
+            <Text>This is the home screen</Text>
+            <Button title="Go to About Screen" />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    center: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+    },
+});
+
+export default Home;
