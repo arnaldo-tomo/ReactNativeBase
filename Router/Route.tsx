074c880ab9ca397ec4c15241827d48f2d7cebcb2
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-17 19:50:03 +0200

    Update Route.tsx

--- Router/Route.tsx
+++ Router/Route.tsx
@@ -1,38 +1,52 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Ionicons } from '@expo/vector-icons';
 import ShowTask from '../screens/ShowTask';
-import CreatTask from '../screens/CreatTask';
 import Home from '../pages/home';
-export default function TabsNavigation() {
-    const Tab = createBottomTabNavigator();
+import Actualizar from '../screens/Actualizar';
+
+const Stack = createNativeStackNavigator();
+function Primeiro() {
     return (
         <NavigationContainer>
-
-            <Tab.Navigator screenOptions={({ route }) => ({
-                tabBarIcon: ({ focused, color, size }) => {
-                    let iconName;
-                    if (route.name === 'Show') {
-                        iconName = focused
-                            ? 'ios-home-outline'
-                            : 'ios-home-outline';
-                    } else if (route.name === 'CreatTask') {
-                        iconName = focused ?
-                            'ios-list-outline' :
-                            'ios-list-outline';
-                    }
-                    return <Ionicons name={iconName} size={size} color={color} />;
-                },
-
-                tabBarActiveTintColor: 'blue',
-                tabBarInactiveTintColor: 'black',
-            })}
-            >
-                <Tab.Screen name="Show" component={ShowTask} />
-                <Tab.Screen name="CreatTask" component={Home} />
-            </Tab.Navigator>
+            <Stack.Navigator>
+                <Stack.Screen name="Tabs" component={TabsNavigation} options={{ headerShown: false }} />
+                <Stack.Screen name="Actualizar" component={Actualizar} />
+            </Stack.Navigator>
         </NavigationContainer>
+    );
+}
+
+const Tab = createBottomTabNavigator();
+function TabsNavigation() {
+    return (
+        <Tab.Navigator screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+                if (route.name === 'Show') {
+                    iconName = focused
+                        ? 'ios-home-outline'
+                        : 'ios-home-outline';
+                } else if (route.name === 'CreatTask') {
+                    iconName = focused ?
+                        'ios-list-outline' :
+                        'ios-list-outline';
+                }
+                return <Ionicons name={iconName} size={size} color={color} />;
+            },
+
+            tabBarActiveTintColor: 'blue',
+            tabBarInactiveTintColor: 'black',
+        })}
+        >
+            <Tab.Screen name="Show" component={ShowTask} />
+            <Tab.Screen name="CreatTask" component={Home} />
+        </Tab.Navigator>
 
     );
 }
+
+
+export { Primeiro };
