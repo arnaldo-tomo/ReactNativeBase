fc2d3724290414e51c8d8f389f44b7a50af64931
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-18 16:05:17 +0200

    Update CreatTask.jsx

--- screens/CreatTask.jsx
+++ screens/CreatTask.jsx
@@ -1,39 +1,114 @@
 import React from 'react';
-import { NativeBaseProvider, Box, FormControl, Input, Fab, Icon, Center, Heading, VStack, Link, Button, HStack, Text } from "native-base";
-import { AntDesign } from "@expo/vector-icons";
-import { View, TextInput, Alert } from "react-native";
-import { useForm, Controller } from "react-hook-form";
-type FormData = {
-    firstName: string;
-    lastName: string;
-};
+import axios from 'axios';
+import { Formik } from 'formik';
+import { NativeBaseProvider, Box, FormControl, Input, Alert, IconButton, Stack, Icon, Center, Heading, VStack, Collapse, Button, HStack, Text, Hidden } from "native-base";
+import { useState } from 'react';
 
-export default function CreatTask() {
-    const { register, setValue, handleSubmit, formState: { errors } } = useForm < FormData > ();
-    const onSubmit = handleSubmit(data => console.log(data));
+export default function CreatTask({ route }) {
+    const [show, setShow] = React.useState(false);
+
+    const [errorMessage, setErrorMessage] = useState([]);
+
+    const { id, nome, curso, email, telefone } = route.params;
+    const fomulario = {
+        id: id,
+        nome: nome,
+        email: email,
+        curso: curso,
+        telefone: telefone,
+    }
     return (
         <NativeBaseProvider>
-            <Box safeArea p="2" py="8" >
-                <Heading size="lg" fontWeight="600" color="coolGray.800" _dark={{ color: "warmGray.50" }}>
-                    Welcome
-                </Heading>
 
-                <VStack space={2} mt="5" onSubmit={onSubmit}>
+            <Center w="100%">
+                <Box safeArea p="2" w="90%" maxW="290" py="8">
+                    <Heading size="lg" color="coolGray.800" _dark={{
+                        color: "warmGray.50"
+                    }} fontWeight="semibold">
+                        Restfull API Update
+                    </Heading>
 
-                    <FormControl>
-                        <FormControl.Label>Email ID</FormControl.Label>
-                        <Input text="text" {...register("firstName")} />
-                    </FormControl>
+                    <Heading mt="1" color="coolGray.600" _dark={{
+                        color: "warmGray.200"
+                    }} fontWeight="medium" size="xs">
+                        Update
+                    </Heading>
 
-                    <FormControl>
-                        <FormControl.Label>Password</FormControl.Label>
-                        <Input type="text"{...register("lastName")} />
-                    </FormControl>
 
-                    <Button type="submit" onPress={handleSubmit(onSubmit)} mt="2" colorScheme="indigo"> Salvar </Button>
+                    {/* Alerta */}
+                    <Collapse isOpen={show}>
+                        <Alert maxW="400" status="error">
+                            <VStack space={1} flexShrink={1} w="100%">
+                                <HStack flexShrink={1} space={2} alignItems="center" justifyContent="space-between">
+                                    <HStack flexShrink={1} space={2} alignItems="center">
+                                        <Alert.Icon />
+                                        <Text fontSize="md" fontWeight="medium" _dark={{
+                                            color: "primary.900"
+                                        }}>
+                                            Please try again later!
+                                        </Text>
+                                    </HStack>
 
-                </VStack>
-            </Box>
-        </NativeBaseProvider>
+                                </HStack>
+                                <Box pl="6" _dark={{
+                                    _text: {
+                                        color: "coolGray.600"
+                                    }
+                                }}>
+                                    {errorMessage ? <Text>{errorMessage}</Text> : null}
+                                </Box>
+                            </VStack>
+                        </Alert>
+                    </Collapse>
+                    {/* Fim da alerta */}
+
+                    <Formik
+
+                        initialValues={fomulario} onSubmit={values => axios.post("http://127.0.0.1:8000/api/update2", values)
+
+                            .then(() => {
+                                navigation.navigate('Tabs',);
+                            })
+                            .catch((error) => {
+                                setShow(true);
+                                setErrorMessage(error.response.data.message);
+
+                            })
+                        }
+                    >
+
+
+                        {({ handleChange, handleBlur, handleSubmit, values }) => (
+
+                            <VStack space={3} mt="5">
+                                <Hidden>
+                                    <Input Disabled="true" onChangeText={handleChange('id')} onBlur={handleBlur('id')} value={values.id} />
+                                </Hidden>
+
+                                <FormControl>
+                                    <FormControl.Label>nome</FormControl.Label>
+                                    <Input onChangeText={handleChange('nome')} onBlur={handleBlur('nome')} value={values.nome} />
+                                </FormControl>
+                                <FormControl>
+                                    <FormControl.Label>Curso</FormControl.Label>
+                                    <Input onChangeText={handleChange('curso')} onBlur={handleBlur('curso')} value={values.curso} />
+                                </FormControl>
+                                <FormControl>
+                                    <FormControl.Label>email</FormControl.Label>
+                                    <Input onChangeText={handleChange('email')} onBlur={handleBlur('email')} value={values.email} />
+                                </FormControl>
+                                <FormControl>
+                                    <FormControl.Label>telefone</FormControl.Label>
+                                    <Input onChangeText={handleChange('telefone')} onBlur={handleBlur('telefone')} value={values.telefone} />
+                                </FormControl>
+                                <Button onPress={handleSubmit} mt="2" colorScheme="indigo">  Salvar </Button>
+                            </VStack>
+                        )}
+                    </Formik>
+                </Box>
+            </Center>
+
+
+        </NativeBaseProvider >
     )
 }
