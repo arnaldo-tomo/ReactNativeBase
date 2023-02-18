0025a0ea953a635b25cc992ba4fb3d9a84be0508
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-18 18:20:25 +0200

    Update ShowTask.jsx

--- screens/ShowTask.jsx
+++ screens/ShowTask.jsx
@@ -1,39 +1,97 @@
 import axios from 'axios';
-import { React } from 'react';
-import { useEffect, useState } from 'react';
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 import { AntDesign } from "@expo/vector-icons";
+import { View, Text, RefreshControl } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import { NativeBaseProvider, HStack, Box, Text, ScrollView, VStack, FlatList, Button } from 'native-base';
+import { NativeBaseProvider, HStack, Box, ScrollView, VStack, FlatList, Button, Collapse, Alert, Spinner, Heading } from 'native-base';
 
 export default function ShowTask({ navigation, route }) {
     const [show, setShow] = React.useState(false);
     const [errorMessage, setErrorMessage] = useState([]);
-    const [task, taskse] = useState([]);
+    const [data, setData] = useState();
 
     useEffect(() => {
-
-        axios.get("http://127.0.0.1:8000/api/todos")
-            .then((response) => {
-                taskse(response.data);
-                console.log(response);
-
-            })
+        fetchData();
     }, []);
 
+    function fetchData() {
+
+        axios.get('http://127.0.0.1:8000/api/todos')
+
+            .then((response) => {
+                setData(response.data)
+            })
+
+            .catch(() => {
+
+            })
+    };
 
     function apagar(id) {
+
         axios.post(`http://127.0.0.1:8000/api/delete/${id}`)
+
             .then((response) => {
-                navigation.navigate('Show');
-                console.log('esse gajo foi elimi multiTanes nado  digitalhost ', response);
+                fetchData();
+                setShow(true);
+                console.log('Eliminado', id);
             })
+            .catch(() => {
+
+            })
+    }
+
+    if (!data) {
+        return (
+            <NativeBaseProvider>
+                <HStack space={4} paddingTop="500" justifyContent="center" alignItems="center">
+                    <Spinner color="warning.500" size="lg" />
+                    <Heading color="warning.500" fontSize="md">
+                        Carregando...
+                    </Heading>
+                </HStack>
+            </NativeBaseProvider>
+        )
+
     }
     return (
 
         <NativeBaseProvider>
-            <ScrollView>
+            {/* Alerta */}
+            <Collapse isOpen={show}>
+                <Alert maxW="400" status="success">
+                    <VStack space={1} flexShrink={1} w="100%">
+                        <HStack flexShrink={1} space={2} alignItems="center" justifyContent="space-between">
+                            <HStack flexShrink={1} space={2} alignItems="center">
+                                <Alert.Icon />
+                                <Text fontSize="md" fontWeight="medium" _dark={{
+                                    color: "primary.900"
+                                }}>
+                                    Task  Eliminado!
+                                </Text>
+                            </HStack>
+                        </HStack>
+                        <Box pl="6" _dark={{
+                            _text: {
+                                color: "coolGray.600"
+                            }
+                        }}>
+                            {errorMessage}
+                        </Box>
+                    </VStack>
+                </Alert>
+            </Collapse>
+            {/* Fim da alerta */}
+            <ScrollView
+            >
+
                 <Box>
-                    <FlatList data={task} renderItem={({ item }) =>
+
+                    <FlatList data={data} renderItem={({ item }) =>
+
                         <Box borderBottomWidth="1" _dark={{ borderColor: "muted.50" }}
                             borderColor="muted.800" pl={["0", "4"]} pr={["0", "5"]} py="2">
                             <HStack space={[4, 4]} marginX="4" justifyContent="space-between">
