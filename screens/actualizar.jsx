cadb06a5c0b44ac70bc7276f7c044d61a3b39be5
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-18 16:05:15 +0200

    Update actualizar.jsx

--- screens/actualizar.jsx
+++ screens/actualizar.jsx
@@ -1,12 +1,18 @@
-import { NativeBaseProvider, Text } from 'native-base';
-export default function Actualizar({ route }) {
+import React from 'react';
+import { NativeBaseProvider, Box, FormControl, Input, Fab, Icon, Center, Heading, VStack, Link, Button, HStack, Text } from "native-base";
 
-    const { id } = route.parms;
+export default function Actualizar({ route }) {
+    const { nome } = route.params;
+
     return (
         <NativeBaseProvider>
-            <Text>
-                Tela de update nao ::: {id}
-            </Text>
+            <Box safeArea p="2" py="8" >
+                <Heading size="lg" fontWeight="600" color="coolGray.800" _dark={{ color: "warmGray.50" }}>
+                    Welcome {nome}
+                </Heading>
+
+
+            </Box>
         </NativeBaseProvider>
     )
 }
